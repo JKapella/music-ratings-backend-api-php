4bda4882337198bfededbc7143bf19f70586b15a
Jkapella <jvkapella@gmail.com>
2019-07-13 10:44:39 +0100

    added album resource collection, to create index in controller

--- music-ratings-api/app/Http/Controllers/AlbumController.php
+++ music-ratings-api/app/Http/Controllers/AlbumController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Album;
 use App\Http\Resources\AlbumResource;
+use App\Http\Resources\AlbumResourceCollection;
 
 class AlbumController extends Controller
 {
@@ -16,5 +17,10 @@ class AlbumController extends Controller
     public function show(Album $album): AlbumResource
     {
         return new AlbumResource($album);
+    }
+
+    public function index(): AlbumResourceCollection 
+    {
+
     }
 }
